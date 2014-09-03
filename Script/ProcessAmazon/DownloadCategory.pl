d067558fd8fe73c7f52ea27b43639555212d65c1
Qi Zhao <manazhao@gmail.com>
2014-09-03 15:24:40 -0700

    more on download

--- Script/ProcessAmazon/DownloadCategory.pl
+++ Script/ProcessAmazon/DownloadCategory.pl
@@ -42,6 +42,11 @@ GetOptions("node-id=s" => \$node_id, "node-name=s" => \$node_name, "local-wd=s" 
 
 $node_id  and $node_name and $local_wd and $remote_wd and $cluster_user or usage();
 
+#my $pid = printAndExecute(remoteCommand("irkm-1.soe.ucsc.edu","ls / 1>/dev/null 2>&1 & echo \$!"));
+#my $pid = qx'ssh manazhao@irkm-1 "ls / 1>/dev/null 2>&1 & echo \$!"';
+#print $pid . "\n";
+#exit;
+
 # get category path
 $local_wd .= "/$node_name";
 
@@ -86,7 +91,7 @@ if(not remoteFileExist($main_node,getFullRemotePath(CATEGORY_TREE_FILE))){
 
 # generate leaf category file
 if(not remoteFileExist($main_node,getFullRemotePath(LEAF_CATEGORY_FILE))){
-    my $perl_cmd = 'perl -lne \'chomp;  if(/(^[^\t]+)\t$/){print $1}\'  ' . getFullRemotePath(CATEGORY_TREE_FILE) . " > " . getFullRemotePath(LEAF_CATEGORY_FILE);
+    my $perl_cmd = 'perl -lne "chomp;  if(/(^[^\t]+)\t$/){print \$1}"  ' . getFullRemotePath(CATEGORY_TREE_FILE) . " > " . getFullRemotePath(LEAF_CATEGORY_FILE);
     $cmd = remoteCommand($main_node,$perl_cmd);
     printAndExecute($cmd);
 }else{
@@ -172,7 +177,7 @@ foreach my $node_idx( 0 .. $#cluster_node_list){
     my $node_wd = getFullRemotePath(LEAF_CATEGORY_DIR);
     my $node_target = FILE_SPLIT_PREFIX . $node_file_suffix_map[$node_idx];
     my $log = getFullRemotePath(LEAF_CATEGORY_DIR) . "/nohup_process_crawl.log";
-    my $remote_cmd = remoteCommand($node, "nohup ProcessCrawledPage.pl --wd=$node_wd --target=$node_target --parser=Amazon/CategoryPageParser 1>$log 2>&1 & echo ". '$!');
+    my $remote_cmd = remoteCommand($node, "nohup ProcessCrawledPage.pl --wd=$node_wd --target=$node_target --parser=Amazon/CategoryPageParser 1>$log 2>&1 & echo \$!" );
     my $pid = printAndExecute($remote_cmd);
     if($pid){
         print ">>> ProcessCrawledPage.pl on $node: $pid\n";
@@ -187,123 +192,166 @@ if(scalar keys %node_pid_map){
     waitClusterFinish(\%node_pid_map);
 }
 
+# extract asins
+		
+my $local_asin_file = getFullLocalPath(PRODUCT_DIR) . "/asin.csv";
+%node_pid_map = ();
+if(not -f $local_asin_file){
+	print ">>> extract asin from category product pages\n";
+	# extract on each node
+	foreach my $node_idx(0 .. $#cluster_node_list){
+		my $node = $cluster_node_list[$node_idx];
+		my $node_file = FILE_SPLIT_PREFIX . $node_file_suffix_map[$node_idx];
+		my $input_file = getFullRemotePath(LEAF_CATEGORY_DIR) . "/$node_file" . "_parsed.json";
+		my $output_file = getFullRemotePath(LEAF_CATEGORY_DIR) . "/$node_file" . "_asin.csv";
+		my $perl_cmd = 'perl -lne "while(/\"asin\":\"([\d\w]+)\"/g){ print \$1}" ' . $input_file . " > $output_file 2>/dev/null & echo \$!";
+		my $remote_cmd = remoteCommand($node, $perl_cmd);
+		my $pid = printAndExecute($remote_cmd);
+		if($pid){
+			$node_pid_map{$node} = $pid;
+			print ">>> extract asin on $node : $pid\n";
+		}else{
+			print ">>> [err] failed to extract asin on $node\n";
+		}
+	}
+	print ">>> wait for all extraction job done\n";
+	waitClusterFinish(\%node_pid_map);
+	print ">>> concatenate asins of each node to a single file\n";
 
+	# concatenate all asins 
+	my $local_asin_tmp_file = getFullLocalPath(PRODUCT_DIR) . "/asin.tmp.csv";
+	foreach my $node_idx(0 .. $#cluster_node_list){
+		my $node = $cluster_node_list[$node_idx];
+		my $node_file = FILE_SPLIT_PREFIX . $node_file_suffix_map[$node_idx];
+		my $node_asin_file = getFullRemotePath(LEAF_CATEGORY_DIR) . "/$node_file" . "_asin.csv";
+		# do the dump
+		$cmd = "ssh $cluster_user\@$node 'cat $node_asin_file' >> $local_asin_tmp_file";
+		print ">>> $cmd\n";
+		`$cmd`;
+	}	
+	`sort $local_asin_tmp_file |uniq > $local_asin_file`;
+	`rm $local_asin_tmp_file`;
+	# now copy to the $main_node
+	$cmd = "scp $local_asin_file $cluster_user\@$main_node:" . getFullRemotePath(PRODUCT_DIR) . "/";
+	print ">>> $cmd\n";
+	`$cmd`;
+}
 
 # extract product information
 
 sub waitClusterFinish{
-   my($node_pid_map) = @_;
-   my $num_processes = keys %$node_pid_map;
-   while($num_processes){
-      while(my($node,$pid) = each %$node_pid_map){
-	 if(checkRemotePid($node,$pid)){
-	    delete $node_pid_map->{$node};
-	    $num_processes--;
-	 }
-      }
-      # sleep 5 seconds between checks
-      sleep(5);
-   }
+	my($node_pid_map) = @_;
+	my $num_processes = keys %$node_pid_map;
+	while($num_processes){
+		while(my($node,$pid) = each %$node_pid_map){
+			if(checkRemotePid($node,$pid)){
+				delete $node_pid_map->{$node};
+				$num_processes--;
+			}
+		}
+		# sleep 5 seconds between checks
+		sleep(5);
+	}
 }
 
 
 sub remoteWget{
-    my ($host, $input_url_file, $wait) = @_;
-    # get the path
-    my $dir = dirname($input_url_file);
-    my $file_name = basename($input_url_file);
-    my $page_dir = $input_url_file . "_pages";
-    if(!remoteFileExist($host,$page_dir,1)){
-        # create the directory
-        my $cmd = remoteCommand($host, "mkdir $page_dir");
-        printAndExecute($cmd);
-    }
-    my $log_file = "nohup_wget.log";
-    my $wget_cmd = "wget  -i ../$file_name  -o $log_file -t 3 -w $wait -nc -U 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4' ";
-    my $full_cmd = "cd $page_dir; nohup $wget_cmd >$log_file 2>&1 & echo " . '$!';
-    my $remote_cmd = remoteCommand($host, $full_cmd);
-    return printAndExecute($remote_cmd);
+	my ($host, $input_url_file, $wait) = @_;
+	# get the path
+	my $dir = dirname($input_url_file);
+	my $file_name = basename($input_url_file);
+	my $page_dir = $input_url_file . "_pages";
+	if(!remoteFileExist($host,$page_dir,1)){
+		# create the directory
+		my $cmd = remoteCommand($host, "mkdir $page_dir");
+		printAndExecute($cmd);
+	}
+	my $log_file = "nohup_wget.log";
+	my $wget_cmd = "wget  -i ../$file_name  -o $log_file -t 3 -w $wait -nc -U 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.6) Gecko/20070802 SeaMonkey/1.1.4' ";
+	my $full_cmd = "cd $page_dir; nohup $wget_cmd >$log_file 2>&1 & echo \$! " ;
+	my $remote_cmd = remoteCommand($host, $full_cmd);
+	return printAndExecute($remote_cmd);
 }
 
 sub checkRemotePid{
-   my($host,$pid) = @_;
-   my $cmd = remoteCommand($host,"ps cax|grep \\s*$pid 2>&1 ");
-   my $output = `$cmd`;
-   chomp $output;
-   return $cmd =~ m/$pid/;
+	my($host,$pid) = @_;
+	my $cmd = remoteCommand($host,'ps cax|grep \s*' . $pid . '2>&1 ');
+	my $output = printAndExecute($cmd);
+	chomp $output;
+	return $cmd =~ m/$pid/;
 }
 
 sub remoteFileExist{
-   my($host, $file, $isDir) = @_;
-   my $test = $isDir ? "-d" : "-f";
-   my $ssh_cmd = remoteCommand($host, "[ $test $file ] && echo exists");
-   my $output = printAndExecute($ssh_cmd);
-   return not( $output eq "");
+	my($host, $file, $isDir) = @_;
+	my $test = $isDir ? "-d" : "-f";
+	my $ssh_cmd = remoteCommand($host, "[ $test $file ] && echo exists");
+	my $output = printAndExecute($ssh_cmd);
+	return not( $output eq "");
 }
 
 # download the category page
 
 sub remoteCommand{
-   my($host,$cmd) = @_;
-   return "ssh $cluster_user\@$host \"$cmd\"";
+	my($host,$cmd) = @_;
+	return "ssh $cluster_user\@$host '$cmd'";
 }
 
 
 sub printAndExecute{
-   my($cmd) = @_;
-   print ">>> " . $cmd . "\n";
-   my $cmdOutput = `$cmd`;
-   chomp $cmdOutput;
-   return $cmdOutput;
+	my($cmd) = @_;
+	print ">>> " . $cmd . "\n";
+	my $cmdOutput = `$cmd`;
+	chomp $cmdOutput;
+	return $cmdOutput;
 }
 
 sub getFullRemotePath{
-   my ($path) = @_;
-   return $remote_wd . "/" . $path;
+	my ($path) = @_;
+	return $remote_wd . "/" . $path;
 }
 
 sub getFullLocalPath{
-   my ($path) = @_;
-   return $local_wd . "/" . $path;
+	my ($path) = @_;
+	return $local_wd . "/" . $path;
 }
 
 
 sub rsyncToNode{
-   my ($host,$local_dir,$remote_dir) = @_;
-   my $cmd = "rsync -avrz $local_dir $cluster_user\@$host:$remote_dir";
-   printAndExecute($cmd);
+	my ($host,$local_dir,$remote_dir) = @_;
+	my $cmd = "rsync -avrz $local_dir $cluster_user\@$host:$remote_dir";
+	printAndExecute($cmd);
 }
 
 sub rsyncFromNode{
-   my ($host,$remote_dir,$local_dir) = @_;
-   my $cmd = "rsync -avrz $cluster_user\@$host:$remote_dir $local_dir";
-   printAndExecute($cmd);
+	my ($host,$remote_dir,$local_dir) = @_;
+	my $cmd = "rsync -avrz $cluster_user\@$host:$remote_dir $local_dir";
+	printAndExecute($cmd);
 }
 
 sub rsyncToCluster{
-   my ($local_dir, $remote_dir) = @_;
-   foreach my $node(@cluster_node_list){
-      rsyncToNode($node,$local_dir,$remote_dir);
-   }
+	my ($local_dir, $remote_dir) = @_;
+	foreach my $node(@cluster_node_list){
+		rsyncToNode($node,$local_dir,$remote_dir);
+	}
 }
 
 sub executeCluster{
-   my ($cmd,$bg) = @_;
-   foreach my $node(@cluster_node_list){
-      my $sshCmd = remoteCommand($node,$cmd,$bg);
-      printAndExecute($sshCmd);
-   }
+	my ($cmd,$bg) = @_;
+	foreach my $node(@cluster_node_list){
+		my $sshCmd = remoteCommand($node,$cmd,$bg);
+		printAndExecute($sshCmd);
+	}
 }
 
 
 sub usage{
-   print <<END;
-$0:
+	print <<END;
+	$0:
    --node-id		category Id
    --node-name		category name
    --local-wd		local working directory
    --remote-wd		remote working directory
    --cluster-user 	user to login cluster nodes (passwordless ssh access is required)
 END
-   die $!;
+	die $!;
 }
